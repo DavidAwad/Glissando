a84df9f2c237abc2dcd9332fc50309dc7d5b8fba
David Awad <davidawad64@gmail.com>
2015-10-11 10:15:11 -0400

    lots of small changes

--- README.md
+++ README.md
@@ -1,17 +1,29 @@
 # Glissando
 
-## forked from the fantastic HopHacks project by asivrat1. 
 
-Glissando trains a Bayesian n-gram model by listening to music and remembering important features such as rhythmic patterns and chord progressions. It then generates a new song in that style. First it samples a chord for each measure from the n-gram distribution based on frequently observed chord progressions in its training set. Once it has done that it samples a melody for that measure. The melody is first given a rhythm, again using an n-gram model trained on the dataset. Once the rhythm is set, it samples the pitches for the melody from the notes that constitute the chord for that measure, weighting them by proximity to the previous note, but excluding the previous note and the one that came before it.
+This project generates music. In the fields of computational linguistics and probability, an n-gram is a contiguous sequence of n items from a given sequence of text or speech. The items can be phonemes, syllables, letters, words or base pairs according to the application. The n-grams are collected from a song corpus using the music21 module.
 
 
-Music21 allows me to parse musical scores stored in either mxml format or midi. The n-gram learner was implemented by hand. I used PyPy to get faster performance as training can be slow. Our training dataset currently includes ~400 chorales by Bach, but can be extended much further.
+Admittedly the music is kinda stale.
 
-## Challenges I ran into
+
+## How it works
+
+
+Glissando trains a Bayesian n-gram model by listening to music and remembering important features such as rhythmic patterns and chord progressions. It then generates a new song in that style. First it samples a chord for each measure from the n-gram distribution based on frequently observed chord progressions in its training set.
+
+Once it has done that it samples a melody for that measure. The melody is first given a rhythm, again using an n-gram model trained on the dataset. Once the rhythm is set, it samples the pitches for the melody from the notes that constitute the chord for that measure, weighting them by proximity to the previous note, but excluding the previous note and the one that came before it.
+
+
+Music21 allows us to parse musical scores stored in either mxml format or midi. The n-gram learner was implemented by hand.
+
+###### note: if you'd like this to run faster you might have better luck with [pypy](http://pypy.org/)
+
+## Challenges
 
 There were some issues with getting the music to sound natural due to the sparsity of the dataset. A certain chord will only appear a few times, so it was important that we figure out how to properly share parameters. Also, every song is written in a different key, so we needed a way to normalize the notes and chords. Finally while we were working on the melody module we ran into some issues with making the progression sound natural. When you deconstruct a melody, it's usually a sequence of notes that appear often in a connected string that move from one chord to the next. Simulating this process was especially difficult since if you use any notes outside of the current chord, it will clash.
 
-## Accomplishments that I'm proud of
+## Accomplishments
 
 I'm very proud of the way that we were able to parameterize the model. We found a way to normalize the chords to key signature, and also share parameters for two chords with the same root note that may appear in different octaves or in different inversions (such chords would sound only slightly different to the human ear, and may be used in place of each other without affecting the song).
 
@@ -20,8 +32,10 @@ Also, one big problem with modern computer generated music is that it sounds aim
 Finally, I think the end result sounds very nice, and can actually be listened to quite comfortably for a while.
 
 
-## What's next for Glissando
+## What's next
 
 The original paper that I read on paintings used a neural network model which can automatically extract and combine interesting features. We weren't able to do that for this Hackathon due to constraints on computing power, but I'd be very interested to see if an approach like that would work in the future.
 
 Another important feature of music is the notion of repeated phrases, melodies, and motifs. Right now the model does not learn these repeated structures beyond the n-grams, but it doesn't have a good sense of the broader structure of the piece and when the melodies should be repeated or explored. This is as of yet a completely unsolved problem in computer generated music--even the neural network approach cannot solve it--but I'd be very eager to see if there's a solution somewhere out there.
+
+## Forked from the fantastic HopHacks project by [asivrat1](http://github.com/asivrat1).
--- chords.py
+++ chords.py
@@ -1,26 +1,82 @@
-#!/usr/bin/pypy
+#!/usr/bin/python
 
-# set the path and import things
-import sys
-sys.path.append("/home/asrivat1/")
-import music21
-from music21 import *
 from collections import defaultdict
+import music21
 import random
 import math
+import sys
 
-# the N in n-gram
-N1 = 2
-N2 = 4
 
-# the smoothing factor
-# think of this as a measure of originality
-lamb = 0.1
+def sampleMultinomial(candidates, weights):
+    '''
+        return a subset of candidates based on a random number
+    '''
+    if not candidates or not weights :
+        raise Exception("multinomial passed null")
 
-# modes that we see
-keys = defaultdict(lambda: 0.0)
+    point = random.random()
+    total = 0.0
+    for i in range(0, len(candidates)):
+        total += weights[i]
+        if total > point:
+            return candidates[i]
+    print "Error in sampling"
 
-def trainDurations(score, durations, durvocab):
+
+def train(ngrams, vocab, durations, note_durations):
+    # get list of paths for all midi files you might want
+    # songs = corpus.getBachChorales()
+    songs = music21.corpus.getComposer('Handel')
+    for song in songs:
+        print("Learning {0}".format(song))
+        parseFile(song, ngrams, vocab, durations, note_durations)
+
+
+def parseFile(filename, ngrams, vocab, durations, note_durations):
+    # parse through the given file and adjust our params accordingly
+    key = None
+    mode = None
+    score = music21.corpus.parse(filename)
+
+    # train durations on the notes, not the chords
+    trainDurations(score, durations, note_durations)
+
+    # next train chord ngrams on the chords of the piece
+    chords = score.chordify().flat
+
+    oldChords = ['<BOS>'] * N1
+
+    for c in chords:
+        if "Chord" in c.classes:
+            root = c.root()
+            relativeroot = str(music21.interval.notesToChromatic(key, root).semitones)
+
+            oldChords.pop(0)
+            oldChords.append(relativeroot)
+
+            ngram = '\n'.join(oldChords)
+            ngrams[ngram] += 1
+
+            relativePitches = []
+            for p in c.pitchNames:
+                relativePitches.append(str(music21.interval.notesToChromatic(key, music21.note.Note(p)).semitones))
+
+            notes = " ".join(relativePitches)+'\t'+mode+'\t'+relativeroot
+
+            vocab.add(notes)
+
+        elif "KeySignature" in c.classes:
+            key, mode = c.pitchAndMode
+            if mode is None:
+                mode = "unknown"
+            keySignature = key.name + '\t' + mode
+            keys[keySignature] += 1
+
+
+def trainDurations(score, durations, note_durations):
+    '''
+    takes in a score object, durations and a vocab
+    '''
     oldDurations = ['<BOS>'] * N2
 
     for part in score.parts:
@@ -34,62 +90,8 @@ def trainDurations(score, durations, durvocab):
                         durgram = '\n'.join(oldDurations)
                         durations[durgram] += 1
 
-                        durvocab.add(component.duration.type)
+                        note_durations.add(component.duration.type)
 
-
-def parseFile(filename, ngrams, vocab, durations, durvocab):
-    key = None
-    mode = None
-    score = corpus.parse(filename)
-
-    # train durations on the notes, not the chords
-    trainDurations(score, durations, durvocab)
-
-    # next train chord ngrams on the chords
-    chords = score.chordify().flat
-
-    oldChords = ['<BOS>'] * N1
-
-    for c in chords:
-        if "Chord" in c.classes:
-            root = c.root()
-            relativeroot = str(interval.notesToChromatic(key, root).semitones)
-
-            oldChords.pop(0)
-            oldChords.append(relativeroot)
-
-            ngram = '\n'.join(oldChords)
-            ngrams[ngram] += 1
-
-            relativePitches = []
-            for p in c.pitchNames:
-                relativePitches.append(str(interval.notesToChromatic(key, note.Note(p)).semitones))
-
-            notes = " ".join(relativePitches) + '\t' + mode + '\t' + relativeroot
-
-            vocab.add(notes)
-
-        elif "KeySignature" in c.classes:
-            key, mode = c.pitchAndMode
-            if mode is None:
-                mode = "unknown"
-            keySignature = key.name + '\t' + mode
-            keys[keySignature] += 1
-
-def train(ngrams, vocab, durations, durvocab):
-    #songs = corpus.getCorePaths('xml')
-    songs = corpus.getBachChorales()
-    for song in songs:
-        parseFile(song, ngrams, vocab, durations, durvocab)
-
-def sampleMultinomial(candidates, weights):
-    point = random.random()
-    total = 0.0
-    for i in range(0, len(candidates)):
-        total += weights[i]
-        if total > point:
-            return candidates[i]
-    print "Error in sampling"
 
 def sampleKey():
     candidates = []
@@ -104,21 +106,25 @@ def sampleKey():
 
     return sampleMultinomial(candidates, weights)
 
-def sampleDuration(durations, durvocab, quota, oldDurations):
+
+def sampleDuration(durations, note_durations, quota, oldDurations):
     candidates = []
     weights = []
 
-    for dur in durvocab:
-        qlength = duration.convertTypeToQuarterLength(dur)
-        if dur != "complex" and qlength <= quota and quota % qlength == 0:
+    for dur in note_durations:
+        qlength = music21.duration.convertTypeToQuarterLength(dur)
+
+        if (dur != "complex") and (0 < qlength <= quota) and (quota % qlength == 0):
             candidates.append(dur)
             durgram = '\n'.join(oldDurations) + '\n' + dur
             weights.append(durations[durgram])
 
-    Z = sum(weights)
+    Z = sum(weights) if sum(weights) > 0 else 1
+    # divide each weight by the avg. to normalize.
     weights = [x / Z for x in weights]
 
     return sampleMultinomial(candidates, weights)
+
 
 def samplePitches(ngrams, vocab, oldChords, mode):
     candidates = []
@@ -141,25 +147,26 @@ def samplePitches(ngrams, vocab, oldChords, mode):
 
     return sampleMultinomial(candidates, weights)
 
-def writeMelody(p, durations, durvocab, key, interv):
-    melody = stream.Part()
+
+def writeMelody(p, durations, note_durations, key, interv):
+    melody = music21.stream.Part()
 
     oldDurations = ["<BOS>"] * (N2 - 1)
 
-    previous1 = note.Note(key)
-    previous2 = note.Note(key)
+    previous1 = music21.note.Note(key)
+    previous2 = music21.note.Note(key)
 
     for hmeasure in p:
         hchord = hmeasure[0]
         pitches = hchord.pitches
-        m = stream.Measure()
+        m = music21.stream.Measure()
 
         quota = 4.0
 
         while quota > 0.0:
             # pick a duration
-            dur = sampleDuration(durations, durvocab, quota, oldDurations)
-            quota -= duration.convertTypeToQuarterLength(dur)
+            dur = sampleDuration(durations, note_durations, quota, oldDurations)
+            quota -= music21.duration.convertTypeToQuarterLength(dur)
 
             # pick a pitch from the chord
             options = []
@@ -169,12 +176,12 @@ def writeMelody(p, durations, durvocab, key, interv):
 
             if len(options) == 0:
                 for pitch in pitches:
-                    options.append(interval.Interval(-12).transposePitch(pitch, maxAccidental = 1))
-                    options.append(interval.Interval(12).transposePitch(pitch, maxAccidental = 1))
+                    options.append(music21.interval.Interval(-12).transposePitch(pitch, maxAccidental=1))
+                    options.append(music21.interval.Interval(12).transposePitch(pitch, maxAccidental=1))
 
             penalty = []
             for pitch in options:
-                penalty.append(math.fabs(float(interval.notesToChromatic(previous2, note.Note(pitch)).semitones)))
+                penalty.append(math.fabs(float(music21.interval.notesToChromatic(previous2, music21.note.Note(pitch)).semitones)))
 
             mx = max(penalty)
             weights = [mx - x + 1 for x in penalty]
@@ -182,7 +189,7 @@ def writeMelody(p, durations, durvocab, key, interv):
             weights = [x / Z for x in weights]
 
             pitch = sampleMultinomial(options, weights)
-            n = note.Note(pitch)
+            n = music21.note.Note(pitch)
 
             # transpose for audibility
             n = n.transpose(interv)
@@ -198,45 +205,47 @@ def writeMelody(p, durations, durvocab, key, interv):
 
     return melody
 
-def sampleRhythm(durations, durvocab, quota):
+
+def sampleRhythm(durations, note_durations, quota):
     oldDurations = ['<BOS>'] * (N2 - 1)
     pattern = []
     quota = 4.0
 
     while quota > 0.0:
-        dur = sampleDuration(durations, durvocab, quota, oldDurations)
-        quota -= duration.convertTypeToQuarterLength(dur)
+        dur = sampleDuration(durations, note_durations, quota, oldDurations)
+        quota -= music21.duration.convertTypeToQuarterLength(dur)
         pattern.append(dur)
 
     return pattern
 
-def writeSong(ngrams, vocab, durations, durvocab, timeSig):
+
+def writeSong(ngrams, vocab, durations, note_durations, timeSig):
     keySignature = sampleKey()
     key, mode = keySignature.split()
 
-    s = stream.Score()
-    p = stream.Part()
+    s = music21.stream.Score()
+    p = music21.stream.Part()
 
     # sample a rhythmic pattern
-    pattern = sampleRhythm(durations, durvocab, 4.0)
+    pattern = sampleRhythm(durations, note_durations, 4.0)
 
     # first sample a harmonic baseline of one chord per measure
-
     oldChords = ['<BOS>'] * (N1 - 1)
 
     for i in range(0, 100):
-        m = stream.Measure()
+        m = music21.stream.Measure()
         c = samplePitches(ngrams, vocab, oldChords, mode)
 
         oldChords.pop(0)
         oldChords.append(c.split('\t')[2])
-        
-        # append the sampled chord to streams 
-        absoluteNotes = [note.Note(key).transpose(int(j) - 12) for j in c.split('\t')[0].split()]
-        toAdd = chord.Chord(absoluteNotes)
+
+        # append the sampled chord to streams
+        absoluteNotes = [music21.note.Note(key).transpose(int(j) - 12) for j in c.split('\t')[0].split()]
+        toAdd = music21.chord.Chord(absoluteNotes)
         toAdd.duration.type = "whole"
-        
+
         m.append(toAdd)
+
         '''
         full = True
         for dur in pattern:
@@ -248,62 +257,72 @@ def writeSong(ngrams, vocab, durations, durvocab, timeSig):
             m.append(toAdd)
         '''
 
-        # with some probability, modulate to the dominant or corresponding minor/major
+        # with some probability, modulate to the dominant
+        # corresponding minor/major
         if random.random() > 0.8:
-            key = interval.Interval('p5').transposePitch(music21.pitch.Pitch(key), maxAccidental = 1).name
+            key = music21.interval.Interval('p5').transposePitch(music21.pitch.Pitch(key), maxAccidental=1).name
         elif random.random() < 0.2:
             if mode == "major":
-                key = interval.Interval('M3').transposePitch(music21.pitch.Pitch(key), maxAccidental = 1).name
+                key = music21.interval.Interval('M3').transposePitch(music21.pitch.Pitch(key), maxAccidental=1).name
                 mode = "minor"
             else:
-                key = interval.Interval('m3').transposePitch(music21.pitch.Pitch(key), maxAccidental = 1).name
+                key = music21.interval.Interval('m3').transposePitch(music21.pitch.Pitch(key), maxAccidental=1).name
                 mode = "major"
 
         # every 8 measures, new rhythm
         if (i % 4) == 0:
-            pattern = sampleRhythm(durations, durvocab, 4.0)
+            pattern = sampleRhythm(durations, note_durations, 4.0)
 
         p.append(m)
 
     s.append(p)
 
     # now sample a melody using the chords established
-    melody = writeMelody(p, durations, durvocab, key, 24)
+    melody = writeMelody(p, durations, note_durations, key, 24)
 
     s.append(melody)
 
     return s
 
-def main():
-    # stores the count for every ngram with add lambda smoothing
+
+# the N in n-gram
+N1 = 2
+N2 = 4
+
+# the smoothing factor
+# think of this as a measure of originality
+lamb = 0.1
+
+# modes that we see
+keys = defaultdict(lambda: 0.0)
+
+
+if __name__ == "__main__":
+    # stores the count for every ngram with added lambda smoothing
     ngrams = defaultdict(lambda: lamb)
-
-    # stores the count for every rhythmic ngram with add lambda smoothing
+    # stores the count for every rhythmic ngram with added lambda smoothing
     durations = defaultdict(lambda: lamb)
-
     # stores the full vocabulary of notes we've seen
     vocab = set()
-
     # stores all possible note durations
-    durvocab = set()
+    note_durations = set()
 
     # train the ngrams and vocab
-    print "Training on Entire Corpus"
-    train(ngrams, vocab, durations, durvocab)
-    print "Done training"
+    train(ngrams, vocab, durations, note_durations)
 
     # write a short song
     print "Writing a short song"
-    s = writeSong(ngrams, vocab, durations, durvocab, 4.0)
+
+    s = writeSong(ngrams, vocab, durations, note_durations, 4.0)
     print "Done writing song"
 
     # write output to MIDI file
     print "Writing output to MIDI file"
-    outfile = midi.translate.streamToMidiFile(s)
+    outfile = music21.midi.translate.streamToMidiFile(s)
     outfile.open("output.mid", 'wb')
     outfile.write()
     outfile.close()
-    print "Done writing file"
 
-if __name__ == "__main__":
-    main()
+
+
+    print("\nAnd we're done")
